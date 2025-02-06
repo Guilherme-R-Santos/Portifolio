64656b5a1c7254f2ceabe0f45e7c07de87ef1c52
Guilherme-R-Santos <guilhermersantos99@outlook.com>
2025-02-05 21:18:27 -0300

    modified:   assets/js/api.js 	modified:   assets/js/main.js 	modified:   data/profile.json 	modified:   index.html

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchprofileData() {
-    const url = 'https://raw.githubusercontent.com/Guilherme-R-Santos/Portifolio/refs/heads/main/data/profile.json?token=GHSAT0AAAAAAC5WFOPVBEZFATYQ5FH36UB6Z5CX36Q';
+    const url = 'https://raw.githubusercontent.com/Guilherme-R-Santos/Portifolio/refs/heads/main/data/profile.json?token=GHSAT0AAAAAAC5WFOPUGT6SUHOO7THX2HSGZ5D7W5A';
     const fetching = await fetch(url);
     return await fetching.json();
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,36 @@
 
+
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo');
+    photo.src = profileData.photo;
+    photo.alt = profileData.name;
+
+    const name = document.getElementById('profile.name');
+    name.innerText = profileData.name;
+
+    const job = document.getElementById('profile.job');
+    job.innerText = profileData.job;
+
+    const location = document.getElementById('profile.location');
+    location.innerText = profileData.location;
+
+    const phone = document.getElementById('profile.phone');
+    phone.innerText = profileData.phone;
+    phone.href = `tel:${profileData.phone}`;
+
+    const email = document.getElementById('profile.email');
+    location.innerText = profileData.email;
+    email.href = `mailto:${profileData.email}`;
+
+    // const github = document.getElementById('profile.github');
+    
+    // const linkedin = document.getElementById('profile.linkedin');
+    
+
+}
+
 (async () => {
     const profileData = await fetchprofileData();
-    console.log(profileData);
+    updateProfileInfo(profileData);
     
 })();
-
-
